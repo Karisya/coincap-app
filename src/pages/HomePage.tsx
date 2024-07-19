ab3171cf1b97449329a050151c3419e7a9f199b5
Karina Romanova <rct.romanovaKE@bsu.by>
2024-07-19 10:19:46 +0300

    setup folder pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import CoinTable from "../components/CoinTable"
+import Header from "../components/Header"
+
+
+const HomePage: React.FC = () => { 
+
+    return (
+        <>
+            <Header />
+            <CoinTable/>
+        </>
+    )
+}
+
+export default HomePage
