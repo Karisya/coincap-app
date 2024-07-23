af21f41a3fd5b01b29b8761f857d8c5e4f2124b8
Karina Romanova <rct.romanovaKE@bsu.by>
2024-07-24 01:08:38 +0300

    modifying the calculation of the portfolio value

--- src/components/PortfolioValue/index.tsx
+++ src/components/PortfolioValue/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../../redux/store';
 import { PortfolioCoin } from '../../redux/reducers/portfolioSlice';
+import { fetchApiData } from '../../redux/reducers/apiDataSlice';
 import styles from './index.module.scss';
 
-export const calculateCurrentPortfolioValue = (coins: PortfolioCoin[]): number => {
+export const calculateCurrentPortfolioValue = (coins: PortfolioCoin[], apiData: Record<string, number>): number => {
     let portfolioValue = 0;
     coins.forEach(({ coin, quantity }) => {
-        if (coin && coin.priceUsd) {
-            portfolioValue += parseFloat(coin.priceUsd) * quantity;
-        }
+        const currentPrice = apiData[coin.id] || parseFloat(coin.priceUsd);
+        portfolioValue += currentPrice * quantity;
     });
     return portfolioValue;
 };
@@ -23,12 +23,29 @@ export const calculatePurchasePortfolioValue = (coins: PortfolioCoin[]): number 
 };
 
 const PortfolioValue: React.FC = () => {
+    const dispatch = useDispatch<AppDispatch>();
     const portfolio = useSelector((state: RootState) => state.portfolio.coins);
-    const currentPortfolioValue = calculateCurrentPortfolioValue(portfolio);
+    const apiData = useSelector((state: RootState) => state.apiData.data);
+    const loading = useSelector((state: RootState) => state.apiData.loading);
+    const error = useSelector((state: RootState) => state.apiData.error);
+
+    useEffect(() => {
+        dispatch(fetchApiData());
+    }, [dispatch]);
+
+    if (loading) {
+        return <div className={styles.loading}>Loading...</div>;
+    }
+
+    if (error) {
+        return <div className={styles.error}>{error}</div>;
+    }
+
+    const currentPortfolioValue = calculateCurrentPortfolioValue(portfolio, apiData);
     const purchasePortfolioValue = calculatePurchasePortfolioValue(portfolio);
 
     const difference = currentPortfolioValue - purchasePortfolioValue;
-    const percentageDifference = ((difference / purchasePortfolioValue) * 100).toFixed(2);
+    const percentageDifference = purchasePortfolioValue ? ((difference / purchasePortfolioValue) * 100).toFixed(2) : '0.00';
 
     return (
         <div className={styles.portfolioContainer}>
